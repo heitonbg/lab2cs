4cb691fe068cb7d370febae99d36b057e063f4cb
iwrawww@gmail.com <iwrawww@gmail.com>
2025-02-27 13:58:58 +0700

    more

---
+++ excel.cs
@@ -0,0 +1,28 @@
+﻿﻿using System;
+
+namespace lab2cs {
+  class Excel : Document {
+    string DocumentExcelRowCount, DocumentExcelColumnCount;
+
+    public Excel(string DocumentTitle, string DocumentAuthor, string DocumentKeyWord, string DocumentPath,
+      string DocumentTopic) {
+      this.DocumentTitle = DocumentTitle;
+      this.DocumentAuthor = DocumentAuthor;
+      this.DocumentKeyWord = DocumentKeyWord;
+      this.DocumentPath = DocumentPath;
+      this.DocumentTopic = DocumentTopic;
+
+      Console.WriteLine("Введите количество столбцов: ");
+      DocumentExcelColumnCount = Console.ReadLine();
+
+      Console.WriteLine("Введите количество строк: ");
+      DocumentExcelRowCount = Console.ReadLine();
+    }
+
+    public override void DocumentInfo() {
+      Console.WriteLine($"\nИмя: {DocumentTitle}\nАвтор: {DocumentAuthor}\nКлючевые слова: {DocumentKeyWord}\n" +
+        $"Путь к файлу: {DocumentPath}\nТема документа: {DocumentTopic}\nКоличество строк: {DocumentExcelRowCount}\n" +
+        $"Количество столбцов: {DocumentExcelColumnCount}\n");
+    }
+  }
+}
