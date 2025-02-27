b9d8d24fe1ba61cf84fe4cd1c91a804381a46ad9
iwrawww@gmail.com <iwrawww@gmail.com>
2025-02-27 14:04:48 +0700

    вроде сделал main код

--- Program.cs
+++ Program.cs
@@ -1,15 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace lab2cs
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
+namespace lab2 {
+  class Singletone {
+    private static Singletone instance;
+
+    public static Singletone Instance { 
+      get { 
+        instance = new Singletone(); 
+        return instance; 
+      } 
     }
+
+    enum Types {
+      TXT = 1,
+      Word,
+      Excel,
+      HTML,
+      PDF,
+    }
+
+    string DocumentTitle, DocumentAuthor,
+      DocumentKeyWord, DocumentPath, DocumentTopic;
+
+    public void DataInput() {
+      Console.WriteLine("Выберите формат файла:\n1. TXT\n2. Word\n3. Excel\n4. HTML\n5. PDF");
+      int UserChoice = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Введите Имя файла: ");
+      DocumentTitle = Console.ReadLine();
+
+      Console.WriteLine("Введите автора: ");
+      DocumentAuthor = Console.ReadLine();
+
+      Console.WriteLine("Введите ключевые слова (через пробел): ");
+      DocumentKeyWord = Console.ReadLine();
+
+      Console.WriteLine("Введите путь к файлу: ");
+      DocumentPath = Console.ReadLine();
+
+      Console.WriteLine("Введите основную тему файла: ");
+      DocumentTopic = Console.ReadLine();
+
+      switch (UserChoice) {
+        case (int)Types.TXT:
+          TXT DocumentTXT = new TXT(DocumentTitle, DocumentAuthor, DocumentKeyWord, DocumentPath, DocumentTopic);
+          DocumentTXT.DocumentInfo();
+          break;
+        case (int)Types.Word:
+          Word DocumentWord = new Word(DocumentTitle, DocumentAuthor, DocumentKeyWord, DocumentPath, DocumentTopic);
+          DocumentWord.DocumentInfo();
+          break;
+        case (int)Types.Excel:
+          Excel DocumentExcel = new Excel(DocumentTitle, DocumentAuthor, DocumentKeyWord, DocumentPath, DocumentTopic);
+          DocumentExcel.DocumentInfo();
+          break;
+        case (int)Types.HTML:
+          HTML DocumentHTML = new HTML(DocumentTitle, DocumentAuthor, DocumentKeyWord, DocumentPath, DocumentTopic);
+          DocumentHTML.DocumentInfo();
+          break;
+        case (int)Types.PDF:
+          PDF DocumentPDF = new PDF(DocumentTitle, DocumentAuthor, DocumentKeyWord, DocumentPath, DocumentTopic);
+          DocumentPDF.DocumentInfo();
+          break;
+      }
+
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      Singletone.Instance.DataInput();
+
+      Console.ReadKey();
+    }
+  }
 }
