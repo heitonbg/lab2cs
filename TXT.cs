5221b61bd43b1e5a7e0656d169c51485a834ca37
iwrawww@gmail.com <iwrawww@gmail.com>
2025-02-27 13:54:33 +0700

    FILE UPDATE

--- TXT.cs
+++ TXT.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿﻿using System;
 
-namespace lab2cs
-{
-    class TXT
-    {
+namespace lab2 {
+  class TXT : Document {
+    string DocumentTXTStringCount, DocumentTXTSignCount;
+
+    public TXT(string DocumentTitle, string DocumentAuthor, string DocumentKeyWord, string DocumentPath,
+      string DocumentTopic) {
+      this.DocumentTitle = DocumentTitle;
+      this.DocumentAuthor = DocumentAuthor;
+      this.DocumentKeyWord = DocumentKeyWord;
+      this.DocumentPath = DocumentPath;
+      this.DocumentTopic = DocumentTopic;
+
+      Console.WriteLine("Введите количество символов: ");
+      DocumentTXTSignCount = Console.ReadLine();
+
+      Console.WriteLine("Введите количество строк: ");
+      DocumentTXTStringCount = Console.ReadLine();
     }
+
+    public override void DocumentInfo() {
+      Console.WriteLine($"\nИмя: {DocumentTitle}\nАвтор: {DocumentAuthor}\nКлючевые слова: {DocumentKeyWord}\n" +
+        $"Путь к файлу: {DocumentPath}\nТема документа: {DocumentTopic}\nКоличество строк: {DocumentTXTStringCount}\n" +
+        $"Количество символов: {DocumentTXTSignCount}\n");
+    }
+  }
 }
