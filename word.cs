12262ca9c8d4877c381332fad3c6c4d2e960716c
iwrawww@gmail.com <iwrawww@gmail.com>
2025-02-27 13:52:31 +0700

    написал код

--- word.cs
+++ word.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿﻿using System;
 
-namespace lab2cs
-{
-    class word
-    {
+namespace lab2 {
+  class Word : Document {
+    string DocumentWordPageCount, DocumentWordUsedFonts;
+
+    public Word(string DocumentTitle, string DocumentAuthor, string DocumentKeyWord, string DocumentPath,
+      string DocumentTopic) {
+      this.DocumentTitle = DocumentTitle;
+      this.DocumentAuthor = DocumentAuthor;
+      this.DocumentKeyWord = DocumentKeyWord;
+      this.DocumentPath = DocumentPath;
+      this.DocumentTopic = DocumentTopic;
+
+      Console.WriteLine("Введите количество страниц: ");
+      DocumentWordPageCount = Console.ReadLine();
+
+      Console.WriteLine("Введите используемые шрифты: ");
+      DocumentWordUsedFonts = Console.ReadLine();
     }
+    
+    public override void DocumentInfo() {
+      Console.WriteLine($"\nИмя: {DocumentTitle}\nАвтор: {DocumentAuthor}\nКлючевые слова: {DocumentKeyWord}\n" +
+        $"Путь к файлу: {DocumentPath}\nТема документа: {DocumentTopic}\nКоличество страниц: {DocumentWordPageCount}\n" +
+        $"Используемые шрифты: {DocumentWordUsedFonts}");
+    }
+  }
 }
